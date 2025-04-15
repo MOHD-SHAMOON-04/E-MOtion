3d75f4093c3540974dfe6fe151ea460a81d1dd21
MOHD-SHAMOON-04 <mohdshamoon04@gmail.com>
2025-04-15 21:44:58 +0530

    lo adnan logo working now

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,23 +15,30 @@ const app = express();
 
 // Get the current file's directory
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = dirname(_filename);
+const __dirname = dirname(__filename);
+// console.log(`Current directory: ${__dirname}`);
+
+const publicPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../views');
+// console.log(`Public directory: ${publicPath}`);
+// console.log(`Views directory: ${viewsPath}`);
+
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static and View Setup
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views/pages'));
+app.set('views', `${viewsPath}/pages`);
 
 app.get("/", (req, res) => {
-  res.render("pages/home.ejs");
+    res.render("home.ejs");
 });
 
 app.get("/orders", (req, res) => {
-  res.render("pages/orders.ejs");
+    res.render("orders.ejs");
 });
 
 
